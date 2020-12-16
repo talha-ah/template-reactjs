0009b1a81661822928364b0d0033d95efb5356e2
Talha Ahmad <49340654+talha-ah@users.noreply.github.com>
2020-12-16 23:53:41 +0500

    Linear Progress

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import LinearProgress from "@material-ui/core/LinearProgress";
 
 const useStyles = makeStyles((theme) => ({
   center: {
